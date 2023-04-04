95cd1fca8689c8c1f7d81e1559313d36a1836ead
Egor Dmitriev <egas099@gmail.com>
2023-04-04 13:00:59 +0300

    deploy on gh-pages

--- src/shared/api/imageService.ts
+++ src/shared/api/imageService.ts
@@ -1,5 +1,5 @@
 export const imageService = {
     headerLogo: './assets/headerLogo.svg',
     largeLogo: './assets/largeLogo.svg',
-    loginBack: 'http://localhost:3000/assets/loginBack.png'
+    loginBack: './assets/loginBack.png'
 };
