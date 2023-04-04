483d59632711ea15bc78bf74693088fd18a38002
Egor Dmitriev <egas099@gmail.com>
2023-04-04 10:42:09 +0300

    refactor login page

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,27 +1,45 @@
-import { Card, Image, Layout, Row } from 'antd';
-import LoginForm from '../../Components/LoginForm/LoginForm';
+import { Card, Col, Image, Layout, Row, Space } from 'antd';
+import LoginForm from 'Components/LoginForm/LoginForm';
 import styles from './Login.module.css';
+import { imageService } from 'shared/api/imageService';
+
+const links = ['Главная', 'Конфиденциальность', 'Условия', 'О портале'];
 
 function Login() {
     return (
         <Layout className={styles.fullHeight}>
-            <Row justify="space-between" align="middle">
-                <Row align="middle" justify="center" className={styles.logo}>
+            <Row justify="space-between" align="middle" wrap={false}>
+                <Col className={styles.promo}>
                     <Image
+                        className={styles.promoBack}
                         alt="MediaLand"
-                        src="https://media-exp1.licdn.com/dms/image/C4D0BAQEwQT6McAH44A/company-logo_200_200/0/1641997103739?e=2147483647&v=beta&t=PWBJDO9m2rAKxarHl7qbbFZoiPGdXWBt_AVYTUOaYzM"
+                        src={imageService.loginBack}
+                        preview={false}
+                        data-back
+                    />
+                    <Image
+                        height={'150px'}
+                        alt="MediaLand"
+                        src={imageService.largeLogo}
                         preview={false}
                     />
-                </Row>
+                    <Row className={styles.links}>
+                        {links.map(link => (
+                            <span>{link}</span>
+                        ))}
+                    </Row>
+                </Col>
                 <Card className={styles.loginSide}>
-                    <div>
-                        <h1>Войдите</h1>
-                        <h4>
-                            или <a href="/">зарегистрируйтесь</a>
-                        </h4>
-                    </div>
-                    <LoginForm />
-                    <h4>Забыли пароль или не можете войти?</h4>
+                    <Space className={styles.card} direction="vertical" size="middle">
+                        <div>
+                            <h1>Войдите</h1>
+                            <h4>
+                                или <a href="/">зарегистрируйтесь</a>
+                            </h4>
+                        </div>
+                        <LoginForm />
+                        <span>Забыли пароль или не можете войти?</span>
+                    </Space>
                 </Card>
             </Row>
         </Layout>
--- src/shared/api/imageService.ts
+++ src/shared/api/imageService.ts
@@ -1,3 +1,5 @@
 export const imageService = {
-    headerLogo: "./assets/MdLd_small.bmp"
-}
+    headerLogo: './assets/headerLogo.svg',
+    largeLogo: './assets/largeLogo.svg',
+    loginBack: 'http://localhost:3000/assets/loginBack.png'
+};
